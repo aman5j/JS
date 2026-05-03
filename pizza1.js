3d5c3774a8e0ebd245bf4f538010af9dac95ed06
aman5j <amanprajapati190@gmail.com>
2026-05-03 14:50:28 +0530

    save changes

--- pizza1.js
+++ pizza1.js
@@ -1,6 +1,6 @@
 function display()
 {
     pn.innerHTML=`<h1>${pizza.value}</h1>`
-    pizzapic.src=`/images/${pizza.value}.png`
+    pizzapic.src=`images/${pizza.value}.png`
     pizzapic.width=350
 }
--- pizza2.js
+++ pizza2.js
@@ -2,7 +2,7 @@ function display()
 {   var amount=rate.value*qty.value
     pn.innerHTML=`<h1>${pizza.value}</h2>`
     pizzapic.width=300
-    pizzapic.src=`/images/${pizza.value}.png`
+    pizzapic.src=`images/${pizza.value}.png`
     price.innerHTML=`Rate:${rate.value}`
     quantity.innerHTML=`Qty:${qty.value}`
     amt.innerHTML=`Amount:${amount}` 
